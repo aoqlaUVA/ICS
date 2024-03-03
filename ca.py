9a0600ec5174206c569fd9d27fc891dc2e8ba541
Samuel <samuel-ampadu@outlook.com>
2024-03-03 19:37:17 +0100

    made a few changes, but will definitely change it

--- ca.py
+++ ca.py
@@ -9,6 +9,7 @@ SCORES = {
     ('D', 'D'): (2, 2)
 }
 
+
 class CASim(Model):
     def __init__(self):
         Model.__init__(self)
@@ -16,7 +17,7 @@ class CASim(Model):
         self.t = 0
         self.rule_set = []
         self.config = None
-        
+
         self.population = []  # Population of strategies
         self.fitness_scores = []  # Fitness scores for each strategy
 
@@ -25,21 +26,44 @@ class CASim(Model):
         self.make_param('chromosome_length', 4)
         self.make_param('crossover_rate', 0.7)
         self.make_param('population', 100, self.initialize_population)
-    
+
     def initialize_population(self):
         # Initialize your population of strategies here
-        return [''.join(np.random.choice(['0', '1']) for _ in range(self.chromosome_length)) for _ in range(self.population_size)]
-    
+        return [''.join(np.random.choice(['0', '1'])
+                        for _ in range(self.chromosome_length))
+                for _ in range(self.population_size)]
+
+    def play_game(self, strategy1, strategy2):
+        # Assuming a simple strategy where the last bit decides the move
+        # 'C' for cooperate, 'D' for defect
+        move1 = 'C' if strategy1[-1] == '0' else 'D'
+        move2 = 'C' if strategy2[-1] == '0' else 'D'
+        return SCORES(move1, move2)
+
     def evaluate_fitness(self):
-        # Implement the logic to evaluate the fitness of each strategy
-        pass
-    
+        #
+        self.fitness_scores = [0 for _ in range(self.population_size)]
+        for i in range(self.population_size):
+            for j in range(self.population_size):
+                if i != j:
+                    score1, score2 = self.play_game(
+                        self.population[i], self.population[j])
+                    self.fitness_scores[i] += score1
+
     def select(self):
         # Implement selection logic here
-        pass
-    
+        # A simple selection method, where strategies with higher fitness
+        # have a higher chance of being selected.
+        selected_indices = np.random.choice(self.population_size,
+                                            size=self.population_size,
+                                            replace=True, p=np.array(
+                                                self.fitness_scores)/
+                                            sum(self.fitness_scores))
+        self.population = [self.population[i] for i in selected_indices]
+
     def crossover_and_mutate(self):
         # Implement crossover and mutation logic here
+
         pass
 
     def evolve_strategies(self):
@@ -72,7 +96,7 @@ class CASim(Model):
             init_row = np.zeros(self.width, dtype=int)
             init_row[int(self.width / 2)] = self.k - 1
         return init_row
-    
+
     def reset(self):
         """Initializes the configuration of the cells and converts the entered
         rule number to a rule set."""
@@ -92,7 +116,7 @@ class CASim(Model):
         if not plt.gca().yaxis_inverted():
             plt.gca().invert_yaxis()
         plt.imshow(self.config, interpolation='none', vmin=0, vmax=self.k - 1,
-                cmap=matplotlib.cm.binary)
+                   cmap=matplotlib.cm.binary)
         plt.axis('image')
         plt.title('t = %d' % self.t)
 
@@ -109,9 +133,10 @@ class CASim(Model):
             # Since slices do not support this, we create an array with the
             # indices we want and use that to index our grid.
             indices = [i % self.width
-                    for i in range(patch - self.r, patch + self.r + 1)]
+                       for i in range(patch - self.r, patch + self.r + 1)]
             values = self.config[self.t - 1, indices]
             self.config[self.t, patch] = self.check_rule(values)
+
 
 if __name__ == '__main__':
     sim = CASim()
